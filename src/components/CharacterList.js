ed3c32c3b961b749203b59ac134885c8f36b3312
Vicky <vyue013@gmail.com>
2019-07-26 10:05:05 -0700

    added API requests for characters and locations, including semantic UI styling

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
+    // https://rickandmortyapi.com/api/character/
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res.data.results);
+        setCharacter(res.data.results);
+      })
+      .catch(err => {
+        console.log("Error! Rick and Morty are currently on vacation: ", err);
+      })
   }, [])
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  return (
+    <section className='character-list grid-view'>
+      {character.map((data) => (
+        <Card id={data.id}>
+          <Image src={data.image} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{data.name}</Card.Header>
+            <Card.Meta>{data.species} / {data.status}</Card.Meta>
+            <Card.Description>Location: {data.location.name}</Card.Description>
+            <Card.Description>Origin: {data.origin.name}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              Episodes
+            </a>
+          </Card.Content>
+        </Card>
+      ))}
     </section>
-
+  );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Card } from 'semantic-ui-react'
 
 export default function LocationsList() {
+    const [location, setLocation] = useState([]);
 
+    useEffect(() => {
+        // TODO: Add AJAX/API Request here - must run in `useEffect`
+        // https://rickandmortyapi.com/api/location/
+        // Important: verify the 2nd `useEffect` parameter: the dependancies array!
+        axios.get("https://rickandmortyapi.com/api/location/")
+            .then(res => {
+                console.log(res.data.results);
+                setLocation(res.data.results);
+            })
+            .catch(err => {
+                console.log("Error! Rick and Morty want some privacy: ", err);
+            })
+    }, [])
+
+    return (
+        <section className='character-list grid-view'>
+            {location.map((data) => (
+                <Card id={data.id}>
+                    <Card.Content>
+                        <Card.Header>{data.type}</Card.Header>
+                        <Card.Description>{data.type} - {data.dimension}</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>{data.residents.length} residents</Card.Content>
+                </Card>
+            ))}
+        </section>
+    );
 }
